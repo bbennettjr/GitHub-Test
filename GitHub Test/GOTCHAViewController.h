a40f48efaf517a57a2c463e2d94787c75444a97a
Brad Bennett <hush-hush-gush@users.noreply.github.com>
2016-02-19 21:47:50 -0500

    Add all files via upload bc couldnt merge branches

---
+++ GitHub Test/GOTCHAViewController.h
@@ -0,0 +1,13 @@
+//
+//  GOTCHAViewController.h
+//  GitHub Test
+//
+//  Created by Bradford Bennett Jr on 2/16/16.
+//  Copyright © 2016 Bradford Bennett Jr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GOTCHAViewController : UIViewController
+
+@end
---
+++ GitHub Test/GOTCHAViewController.m
@@ -0,0 +1,38 @@
+//
+//  GOTCHAViewController.m
+//  GitHub Test
+//
+//  Created by Bradford Bennett Jr on 2/16/16.
+//  Copyright © 2016 Bradford Bennett Jr. All rights reserved.
+//
+
+#import "GOTCHAViewController.h"
+
+@interface GOTCHAViewController ()
+
+@end
+
+@implementation GOTCHAViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
